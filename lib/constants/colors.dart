2c60a6dc4941c0f150a7df29fae6b30d82143712
tanay-clickzy <rocks.d.xebec2411@gmail.com>
2024-02-19 11:26:29 +0530

    first

---
+++ lib/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color grey800 = Color(0xff3F3F3F);
+  static const Color black = Color(0xff0000000);
+  static const Color sTitleColor = Color(0xffF7F8FB);
+  static const Color justGrey60 = Color(0xff7D7C88);
+  static const Color justGrey100 = Color(0xff262439);
+  static const Color justGrey40 = Color(0xffD4D3DC);
+  static const Color justGrey20 = Color(0xffEFEFF6);
+  static const Color green1000 = Color(0xff32AE64);
+  static const Color justGrey10 = Color(0xffFCFCFC);
+  
+
+
+}
---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:riders_app/constants/colors.dart';
+import 'colors.dart';
+
+class FontStyles{
+
+  static TextStyle medium14P({Color? color}) => GoogleFonts.poppins(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: color ?? AppColors.grey800,
+    );
+
+
+    static TextStyle splashTitle({Color? color}) => GoogleFonts.poppins(
+      fontSize: 27.5,
+      fontWeight: FontWeight.w600,
+      color: color ?? AppColors.black,
+    );
+
+
+    static TextStyle medium600_P14({Color? color}) => GoogleFonts.poppins(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: color ?? AppColors.black,
+    );
+
+
+    static TextStyle medium400P14({Color? color}) => GoogleFonts.poppins(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: color ?? AppColors.black,
+    );
+
+
+    static TextStyle mediumP16({Color? color}) => GoogleFonts.poppins(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: color ?? AppColors.black,
+    );
+
+
+    static TextStyle med400P14({Color? color}) => GoogleFonts.poppins(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: color ?? AppColors.black,
+    );
+
+
+
+    
+}
